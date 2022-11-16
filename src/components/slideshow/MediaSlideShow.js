9a0f20ca50fe32a2840f1593a9b6f268a068b1ae
ArthurBrockelman <ArthurBrockelman@gmail.com>
2022-11-16 14:03:28 -0500

    Fixed media slide show.

--- src/components/slideshow/MediaSlideShow.js
+++ src/components/slideshow/MediaSlideShow.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import tasteDiveGetRelatedMedia from "../../utils/tasteDiveGetRelatedMedia"
 import {tasteDiveResults} from "../../utils/tasteDiveResults"
 import YoutubeEmbed from './YoutubeEmbed';
+import { getUserEmail } from '../../utils/authHelper';
+
 class MediaSlideshow extends Component {
 
     state = {
